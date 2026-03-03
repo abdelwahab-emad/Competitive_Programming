7b03fd7a569ea32115406e9ffac5d92bca7769d5
AbdelwahabEmad <abd916587@gmail.com>
2026-03-03 02:54:47 +0200

    Solve Problem W: Reach Value

---
+++ Assuit_Recursion/W_Reach_Value.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+void file(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+#define Code_By_Abdelwabab ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define endl "\n"
+#define int long long
+#define ll long long
+#define ld long double
+#define sz(s) (int)(s).size()
+bool Reach_Value(int cur_val, int n){
+    if(cur_val >= n){
+        return cur_val == n;
+    }
+    return Reach_Value(cur_val * 10, n) or Reach_Value(cur_val * 20, n);
+}
+void solve() {
+    int n;
+    cin >> n;
+    cout << (Reach_Value(1, n) ? "YES" : "NO") << endl;
+}
+signed main() {
+    Code_By_Abdelwabab
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
